5b01a2b4582494234758083f8dd72cfb15012ae4
ALFONSO EDUARDO MARQUEZ CHAMORRO <amarquez6@us.es>
2026-04-08 12:01:46 +0200

    Update Video.java

--- src/main/java/aiss/videominer/model/Video.java
+++ src/main/java/aiss/videominer/model/Video.java
@@ -30,7 +30,7 @@ public class Video {
     @NotEmpty(message = "Video release time cannot be empty")
     private String releaseTime;
 
-    @JsonProperty("author")
+    @JsonProperty("user")
     @OneToOne(cascade = CascadeType.ALL)
     private User author;
 
